4acd2ee6142a8d6fcd658c7bd7b3786caad719d1
diegoseg15 <diegofersv@gmail.com>
2025-03-12 16:45:31 +0100

    :tada: New Project in SpringBoot

---
+++ src/main/java/com/diegoseg15/urgencias/UrgenciasApplication.java
@@ -0,0 +1,13 @@
+package com.diegoseg15.urgencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrgenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrgenciasApplication.class, args);
+	}
+
+}
